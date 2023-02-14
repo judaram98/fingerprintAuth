d23f3feca20dc322e68c7af24d6deb5811900d79
Juan David <judaram98@gmail.com>
2023-02-14 11:44:10 -0500

    Creacion del login e instalacion de fingerprintjs

--- src/Components/Pages/Index/Index.jsx
+++ src/Components/Pages/Index/Index.jsx
@@ -1,10 +1,38 @@
-import styles from "./Index.module.css";
+import { useVisitorData } from "@fingerprintjs/fingerprintjs-pro-react";
+import { useEffect } from "react";
+import Logo from "../../Assets/Images/Logo.png";
 
 const Index = () => {
+  // useEffect(() => {
+  //   FingerprintJS.load({
+  //     token: BROWSER_API_KEY,
+  //   })
+  //     .then((fp) => fp.get())
+  //     .then((result) => {
+  //       setVisitorId(result.visitorId);
+  //       setIsLoading(false);
+  //     });
+  // }, []);
   let HtmlIndex = (
-    <div className={`${styles.container} border-2 border-red-500`}>
-      <h1 className="text-3xl font-bold underline">Index</h1>
-    </div>
+    <form
+      action=""
+      className="w-full h-screen flex flex-col items-center justify-center"
+    >
+      <img src={Logo} alt="" className="h-32 mb-5" />
+      <input
+        type="text"
+        placeholder="Usuario"
+        className="bg-slate-100 rounded-lg p-2 text-center my-1"
+      />
+      <input
+        type="password"
+        placeholder="Contraseña"
+        className="bg-slate-100 rounded-lg p-2 text-center my-1"
+      />
+      <button className="bg-emerald-200 rounded-lg py-2 px-7 my-1 text-emerald-700">
+        Ingresar
+      </button>
+    </form>
   );
   return HtmlIndex;
 };
