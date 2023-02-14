515de003137e007b41deb7bd6420703a7087bf3c
Juan David <judaram98@gmail.com>
2023-02-14 11:50:45 -0500

    correciones, esta version es estable

--- src/index.js
+++ src/index.js
@@ -2,15 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import SystemRoutes from "./Routes/SystemRoutes";
-import { FpjsProvider } from "@fingerprintjs/fingerprintjs-pro-react";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <FpjsProvider
-    loadOptions={{
-      apiKey: "xTa6pK8l8gy9vN3eoqUA",
-    }}
-  >
+  <React.StrictMode>
     <SystemRoutes />
-  </FpjsProvider>
+  </React.StrictMode>
 );
